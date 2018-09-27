1de7df1ed91f6a936fef717aedc1395f5b4131fd
radriangz <radriangz@outlook.com>
2018-09-27 15:03:28 -0500

    Se alinearon los botones de la calculadora y se corigieron algunos tamaños disparejos, se añadieron algunos comentarios al código.

--- src/calculator/CalculatorWithSwing.java
+++ src/calculator/CalculatorWithSwing.java
@@ -24,9 +24,15 @@ public class CalculatorWithSwing {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+					/*Intenta realizar lo que sea que le indique el código
+					 * */
 					CalculatorWithSwing window = new CalculatorWithSwing();
 					window.frame.setVisible(true);
 				} catch (Exception e) {
+					/*atrapa la excepción, un código que hace que el código 
+					 * deje de funcionar, o sea, un error, el catch utiliza
+					 * las líneas de código que tiene para esto.
+					 * */
 					e.printStackTrace();
 				}
 			}
@@ -44,12 +50,12 @@ public class CalculatorWithSwing {
 	 * Inicializa el contenido del frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 328, 426);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frame = new JFrame(); //Se crea el objeto tipo JFrame de nombre frame
+		frame.setBounds(100, 100, 328, 426); //Se establecen las medidas del frame.
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//se establece como cerrar la app
+		frame.getContentPane().setLayout(null);//Se crean los componentes y se colocan dentrodel mismo
 		
-		pantallaCalculadora = new JTextField();
+		pantallaCalculadora = new JTextField();//cuadro de texto, con todos sus datos
 		pantallaCalculadora.setBounds(11, 11, 291, 43);
 		pantallaCalculadora.setHorizontalAlignment(SwingConstants.RIGHT);
 		pantallaCalculadora.setFont(new Font("Tahoma", Font.PLAIN, 30));
@@ -58,12 +64,12 @@ public class CalculatorWithSwing {
 		
 		JButton botonPuntoDecimal = new JButton(".");
 		botonPuntoDecimal.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		botonPuntoDecimal.setBounds(158, 319, 64, 57);
+		botonPuntoDecimal.setBounds(159, 315, 70, 52);
 		frame.getContentPane().add(botonPuntoDecimal);
 		
 		JButton botonIgual = new JButton("=");
 		botonIgual.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		botonIgual.setBounds(232, 256, 70, 120);
+		botonIgual.setBounds(232, 256, 70, 111);
 		frame.getContentPane().add(botonIgual);
 		
 		JButton botonSuma = new JButton("+");
@@ -73,7 +79,7 @@ public class CalculatorWithSwing {
 		
 		JButton botonResta = new JButton("-");
 		botonResta.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		botonResta.setBounds(232, 131, 70, 51);
+		botonResta.setBounds(232, 133, 70, 52);
 		frame.getContentPane().add(botonResta);
 		
 		JButton botonDelete = new JButton("Del");
@@ -83,67 +89,67 @@ public class CalculatorWithSwing {
 		
 		JButton botonDivision = new JButton("/");
 		botonDivision.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		botonDivision.setBounds(158, 66, 64, 52);
+		botonDivision.setBounds(158, 68, 70, 52);
 		frame.getContentPane().add(botonDivision);
 		
 		JButton botonMultiplicacion = new JButton("*");
 		botonMultiplicacion.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		botonMultiplicacion.setBounds(84, 68, 64, 52);
+		botonMultiplicacion.setBounds(84, 68, 70, 52);
 		frame.getContentPane().add(botonMultiplicacion);
 		
 		JButton botonClear = new JButton("C");
 		botonClear.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		botonClear.setBounds(10, 66, 64, 52);
+		botonClear.setBounds(10, 68, 70, 52);
 		frame.getContentPane().add(botonClear);
 		
 		JButton botonNumCero = new JButton("0");
 		botonNumCero.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		botonNumCero.setBounds(10, 319, 138, 57);
+		botonNumCero.setBounds(11, 315, 145, 52);
 		frame.getContentPane().add(botonNumCero);
 		
 		JButton botonNumUno = new JButton("1");
 		botonNumUno.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		botonNumUno.setBounds(10, 256, 64, 52);
+		botonNumUno.setBounds(10, 256, 70, 52);
 		frame.getContentPane().add(botonNumUno);
 		
 		JButton botonNumDos = new JButton("2");
 		botonNumDos.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		botonNumDos.setBounds(84, 256, 64, 52);
+		botonNumDos.setBounds(84, 256, 70, 52);
 		frame.getContentPane().add(botonNumDos);
 		
 		JButton botonNumTres = new JButton("3");
-		botonNumTres.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		botonNumTres.setBounds(158, 256, 64, 52);
+		botonNumTres.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		botonNumTres.setBounds(158, 256, 70, 52);
 		frame.getContentPane().add(botonNumTres);
 		
 		JButton botonNumCuatro = new JButton("4");
 		botonNumCuatro.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		botonNumCuatro.setBounds(10, 193, 64, 52);
+		botonNumCuatro.setBounds(10, 193, 70, 52);
 		frame.getContentPane().add(botonNumCuatro);
 		
 		JButton botonNumCinco = new JButton("5");
 		botonNumCinco.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		botonNumCinco.setBounds(84, 193, 64, 52);
+		botonNumCinco.setBounds(84, 193, 70, 52);
 		frame.getContentPane().add(botonNumCinco);
 		
 		JButton botonNumSeis = new JButton("6");
 		botonNumSeis.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		botonNumSeis.setBounds(158, 193, 64, 52);
+		botonNumSeis.setBounds(158, 193, 70, 52);
 		frame.getContentPane().add(botonNumSeis);
 		
 		JButton botonNumSiete = new JButton("7");
 		botonNumSiete.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		botonNumSiete.setBounds(10, 129, 64, 52);
+		botonNumSiete.setBounds(10, 133, 70, 52);
 		frame.getContentPane().add(botonNumSiete);
 		
 		JButton botonNumOcho = new JButton("8");
 		botonNumOcho.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		botonNumOcho.setBounds(84, 130, 64, 52);
+		botonNumOcho.setBounds(84, 133, 70, 52);
 		frame.getContentPane().add(botonNumOcho);
 		
 		JButton botonNumNueve = new JButton("9");
 		botonNumNueve.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		botonNumNueve.setBounds(158, 129, 64, 52);
+		botonNumNueve.setBounds(158, 133, 70, 52);
 		frame.getContentPane().add(botonNumNueve);
 	}
 }
